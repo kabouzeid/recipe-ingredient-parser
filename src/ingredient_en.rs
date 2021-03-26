a8374e0d7ce13da9d3ca74da5e5924645c43869b
Karim Abou Zeid <karim23697@gmail.com>
2021-03-27 00:43:20 +0100

    Progress

--- src/ingredient_en.rs
+++ src/ingredient_en.rs
@@ -2,14 +2,14 @@ use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "grammar_en.pest"]
-struct IngreedientParser;
+struct IngreedientParserEn;
 
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct Span {
     pub from: usize,
-    pub to: usize
+    pub to: usize,
 }
 
 #[derive(Clone, Copy, PartialEq, Debug, Serialize)]
@@ -17,7 +17,7 @@ pub enum Unit {
     Kilogram,
     Pound,
     Ounce,
-    Cup
+    Cup,
 }
 
 #[derive(Debug, PartialEq, Serialize)]
@@ -40,7 +40,7 @@ pub enum Amount {
 #[derive(Debug, Serialize)]
 pub struct ValueWithSpan<T> {
     value: T,
-    span: Span
+    span: Span,
 }
 
 #[derive(Debug, Serialize)]
@@ -49,11 +49,11 @@ pub struct IngredientInfo {
     pub unit: Option<ValueWithSpan<Unit>>,
     pub container_amount: Option<ValueWithSpan<Amount>>,
     pub container_unit: Option<ValueWithSpan<Unit>>,
-    pub ingredient: Option<ValueWithSpan<String>>
+    pub ingredient: Option<ValueWithSpan<String>>,
 }
 
 pub fn parse(str: &str) -> IngredientInfo {
-    let pairs = IngreedientParser::parse(Rule::ingredient_and_amount_and_unit, str).unwrap();
+    let pairs = IngreedientParserEn::parse(Rule::ingredient_and_amount_and_unit, str).unwrap();
 
     let mut amount: Option<ValueWithSpan<Amount>> = None;
     let mut unit: Option<ValueWithSpan<Unit>> = None;
@@ -65,49 +65,79 @@ pub fn parse(str: &str) -> IngredientInfo {
         match pair.as_rule() {
             Rule::amount => {
                 let span = extract_span(pair.as_span());
-                amount = Some(ValueWithSpan { value: parse_amount(pair), span });
-            },
+                amount = Some(ValueWithSpan {
+                    value: parse_amount(pair),
+                    span,
+                });
+            }
             Rule::unit => {
                 let span = extract_span(pair.as_span());
-                unit = Some(ValueWithSpan { value: parse_unit(pair), span });
-            },
+                unit = Some(ValueWithSpan {
+                    value: parse_unit(pair),
+                    span,
+                });
+            }
             Rule::container_size => {
                 let mut pairs = pair.into_inner();
 
                 let amount_pair = pairs.next().unwrap();
                 let amount_span = extract_span(amount_pair.as_span());
-                container_amount = Some(ValueWithSpan { value: parse_amount(amount_pair), span: amount_span });
+                container_amount = Some(ValueWithSpan {
+                    value: parse_amount(amount_pair),
+                    span: amount_span,
+                });
 
                 let unit_pair = pairs.next().unwrap();
                 let unit_span = extract_span(unit_pair.as_span());
-                container_unit = Some(ValueWithSpan { value: parse_unit(unit_pair), span: unit_span });
-            },
-            Rule::ingredient => {
+                container_unit = Some(ValueWithSpan {
+                    value: parse_unit(unit_pair),
+                    span: unit_span,
+                });
+            }
+            Rule::ingredient | Rule::ingredient_alt => {
                 let span = extract_span(pair.as_span());
-                ingredient = Some(ValueWithSpan { value: pair.as_str().to_string(), span });
-            },
+                ingredient = Some(ValueWithSpan {
+                    value: pair.as_str().to_string(),
+                    span,
+                });
+            }
+            Rule::preposition => continue,
             Rule::EOI => continue,
             _ => unreachable!(),
         }
     }
 
-    IngredientInfo { amount, unit, container_amount, container_unit, ingredient }
+    IngredientInfo {
+        amount,
+        unit,
+        container_amount,
+        container_unit,
+        ingredient,
+    }
 }
 
 fn extract_span(span: pest::Span) -> Span {
-    Span { from: span.start(), to: span.end() }
+    Span {
+        from: span.start(),
+        to: span.end(),
+    }
 }
 
 fn parse_amount(pair: pest::iterators::Pair<Rule>) -> Amount {
     let pair = pair.into_inner().next().unwrap();
     match pair.as_rule() {
-        Rule::constant => Amount::Constant { value: parse_constant(pair) },
+        Rule::constant => Amount::Constant {
+            value: parse_constant(pair),
+        },
         Rule::range => {
             let mut pairs = pair.into_inner();
             let value_from = parse_constant(pairs.next().unwrap());
             let value_to = parse_constant(pairs.next().unwrap());
-            Amount::Range { value_from, value_to }
-        },
+            Amount::Range {
+                value_from,
+                value_to,
+            }
+        }
         _ => unreachable!(),
     }
 }
@@ -147,7 +177,9 @@ fn parse_constant(pair: pest::iterators::Pair<Rule>) -> Constant {
         }
         Rule::float => Constant::Float(pair.as_str().replace(",", ".").parse().unwrap()),
         Rule::integer => Constant::Fraction(pair.as_str().parse().unwrap(), 1),
-        Rule::word_digit => Constant::Fraction(super::dictionary_en::look_up_word_digit(pair.as_str()), 1),
+        Rule::word_digit => {
+            Constant::Fraction(super::dictionary_en::look_up_word_digit(pair.as_str()), 1)
+        }
         _ => unreachable!(),
     }
 }
@@ -177,7 +209,12 @@ mod tests {
             let info = parse("1 1/2 kg potatoes");
 
             let amount = info.amount.unwrap();
-            assert_eq!(amount.value, Amount::Constant { value: Constant::Fraction(3, 2) });
+            assert_eq!(
+                amount.value,
+                Amount::Constant {
+                    value: Constant::Fraction(3, 2)
+                }
+            );
             assert_eq!(amount.span.from, 0);
             assert_eq!(amount.span.to, 5);
 
@@ -191,7 +228,13 @@ mod tests {
             let info = parse("2-3 lb potatoes");
 
             let amount = info.amount.unwrap();
-            assert_eq!(amount.value, Amount::Range { value_from: Constant::Fraction(2,1), value_to: Constant::Fraction(3,1) });
+            assert_eq!(
+                amount.value,
+                Amount::Range {
+                    value_from: Constant::Fraction(2, 1),
+                    value_to: Constant::Fraction(3, 1)
+                }
+            );
             assert_eq!(amount.span.from, 0);
             assert_eq!(amount.span.to, 3);
 
@@ -205,7 +248,12 @@ mod tests {
             let info = parse("1½ cups flour");
 
             let amount = info.amount.unwrap();
-            assert_eq!(amount.value, Amount::Constant { value: Constant::Fraction(3, 2) });
+            assert_eq!(
+                amount.value,
+                Amount::Constant {
+                    value: Constant::Fraction(3, 2)
+                }
+            );
             assert_eq!(amount.span.from, 0);
             assert_eq!(amount.span.to, 3); // because ½ takes two bytes
 
@@ -213,6 +261,48 @@ mod tests {
             assert_eq!(unit.value, Unit::Cup);
             assert_eq!(unit.span.from, 4);
             assert_eq!(unit.span.to, 8);
+        }
+    }
+
+    #[test]
+    fn test_reverse_format() {
+        {
+            let info = parse("Flour 1 kg");
+
+            let amount = info.amount.unwrap();
+            assert_eq!(
+                amount.value,
+                Amount::Constant {
+                    value: Constant::Fraction(1, 1)
+                }
+            );
+            assert_eq!(amount.span.from, 6);
+            assert_eq!(amount.span.to, 7); // because ½ takes two bytes
+
+            let unit = info.unit.unwrap();
+            assert_eq!(unit.value, Unit::Kilogram);
+            assert_eq!(unit.span.from, 8);
+            assert_eq!(unit.span.to, 10);
+        }
+
+        {
+            // (2 kg) is not recognized as container size in reverse
+            let info = parse("Flour (2 kg) 1 kg");
+
+            let amount = info.amount.unwrap();
+            assert_eq!(
+                amount.value,
+                Amount::Constant {
+                    value: Constant::Fraction(1, 1)
+                }
+            );
+            assert_eq!(amount.span.from, 13);
+            assert_eq!(amount.span.to, 14); // because ½ takes two bytes
+
+            let unit = info.unit.unwrap();
+            assert_eq!(unit.value, Unit::Kilogram);
+            assert_eq!(unit.span.from, 15);
+            assert_eq!(unit.span.to, 17);
         }
     }
 }
